a74b9e1985cc40ba5a7c25ccd4477ea1cf053a09
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-09-12 22:37:32 +0300

    app users

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,16 +2,40 @@ from django.shortcuts import render, HttpResponse
 from djoser.views import UserViewSet
 from rest_framework.viewsets import ModelViewSet
 
-from recipes.models import Tag
-from api.serializer import TagSerializer
+from recipes.models import Tag, Recipe
+from api.serializer import (
+    TagSerializer,
+    RecipeSerializer,
+    RecipeCreateSerializer
+)
 
 
 def index(request):
     return HttpResponse('index')
 
+
 class CustomUserViewSet(UserViewSet):
     pass
+
 
 class TagViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    def get_queryset(self):
+        recipes = Recipe.objects.prefetch_related(
+            'recipe_ingredients__ingredient', 'tags'
+        ).all()
+        return recipes
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RecipeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
-from recipes.models import Tag
+from recipes.models import Tag, Recipe, Ingredient, RecipeIngredient
+
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInLine,)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
     pass
